bf4e568d02fde7e6788cb714560628a90fdade2d
Dennis Uckel <dennis.u@gmx.de>
2017-05-17 00:14:53 +0200

    Exercise 5

--- reducer.py
+++ reducer.py
@@ -45,7 +45,8 @@ for line in sys.stdin:
         # to the standart output (stdout)
         # Key and value are seperated by a tab (\t)
         # Line ends with new line (\n)
-        sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+	if sum_of_values>114:
+        	sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
         # Sum of sales starts again with 0
         sum_of_values = 0
 
@@ -58,4 +59,5 @@ for line in sys.stdin:
     previous_key = key
 
 # write the last result to stdout
-sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+if sum_of_values>114:
+	sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
