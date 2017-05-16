cba25e0dfa704d547e3ec3225b0f2792d892b9b0
Dennis Uckel <dennis.u@gmx.de>
2017-05-17 00:25:57 +0200

    Exercise 6

--- reducer.py
+++ reducer.py
@@ -21,6 +21,7 @@ import sys
 
 # Sum of all sales (values) is initialized with zero, we just started
 sum_of_values = 0
+count_of_values = 0
 
 # Previous key is initialized with None, we just started
 previous_key = None
@@ -46,18 +47,20 @@ for line in sys.stdin:
         # Key and value are seperated by a tab (\t)
         # Line ends with new line (\n)
 	if sum_of_values>114:
-        	sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+        	sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values/count_of_values))
         # Sum of sales starts again with 0
         sum_of_values = 0
+	count_of_values = 0
 
     # Add the value to the total sales
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
-    sum_of_values += 1
+    sum_of_values += float(value)
+    count_of_values += 1
     # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
 
 # write the last result to stdout
 if sum_of_values>114:
-	sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+	sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values/count_of_values))
