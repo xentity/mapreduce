45bb85329721248d477508971a3d0c5a1be29565
Dennis Uckel <dennis.u@gmx.de>
2017-05-17 00:04:43 +0200

    Exercise 2

--- reducer.py
+++ reducer.py
@@ -53,7 +53,7 @@ for line in sys.stdin:
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
-    sum_of_values += float(value)
+    sum_of_values += 1
     # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
 
